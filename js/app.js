d49248bf62e5426d92969c4b1a04419631720528
IntiDev <yeseniase@outlook.it>
2018-01-19 15:54:50 -0600

    Se agrega código para 1ra versión de contador dinámico.

--- js/app.js
+++ js/app.js
@@ -1,2 +1,16 @@
-console.log("JS");
 $('.carousel.carousel-slider').carousel({fullWidth: true});
+
+// COUNTERS
+var count1 = document.getElementById('count1');
+var count2 = document.getElementById('count2');
+var count3 = document.getElementById('count3');
+// console.log(count2.innerHTML);
+
+// function counter () {
+  for (var i = 0; i <= 100; i++) {
+    console.log("i");
+    setTimeout(function(){
+      count1.innerHTML = i;
+    }, 1000);
+  }
+// }
