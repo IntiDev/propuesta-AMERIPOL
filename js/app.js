45cd3f7f414ae012c90749b8924d893a8e44df58
IntiDev <yeseniase@outlook.it>
2018-01-19 23:33:06 -0600

    Se agrega funcionalidad a la barra de contadores

--- js/app.js
+++ js/app.js
@@ -5,12 +5,22 @@ var count1 = document.getElementById('count1');
 var count2 = document.getElementById('count2');
 var count3 = document.getElementById('count3');
 // console.log(count2.innerHTML);
+var i = 0;
 
-// function counter () {
-  for (var i = 0; i <= 100; i++) {
-    console.log("i");
-    setTimeout(function(){
-      count1.innerHTML = i;
-    }, 1000);
+var timer = setInterval(function(){
+  count1.innerHTML = ++i;
+  count3.innerHTML = i;
+  if(i == 100){
+    clearInterval(timer);
   }
-// }
+}, 50);
+
+var j = 0;
+
+var timer2 = setInterval(function(){
+  j = j + 3;
+  count2.innerHTML = j;
+  if(j == 300){
+    clearInterval(timer2);
+  }
+}, 100);
